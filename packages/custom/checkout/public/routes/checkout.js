04e550ac03e51b6f5f66a5346c3b6a507ae7aa32
Lucas Lazaro <user.email>
2015-04-08 17:22:35 -0300

    Checkou adjustments

---
+++ packages/custom/checkout/public/routes/checkout.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mean.checkout').config(['$stateProvider',
+  function($stateProvider) {
+    $stateProvider.state('checkout example page', {
+      url: '/checkout/example',
+      templateUrl: 'checkout/views/index.html'
+    });
+  }
+]);
